954a21a0a32a928d85f5c99daf157c209fdb56dd
Aditya Sharma <adityawavnew@gmail.com>
2024-03-10 07:25:11 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-
-connectDB();
+app.on("error", (error) => {
+  console.log("ERROR: app is not able to talk to db", error);
+  throw error;
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed !!!", err);
+  });
 
 /*
 import express from "express";
